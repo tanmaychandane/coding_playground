5c7ec32f5b57dd306c85df66ee26b6cbb71dd30c
Tanmay Chandane <33379183+tanmaychandane@users.noreply.github.com>
2019-01-11 13:42:45 +0530

    Create border_layout.java

---
+++ border_layout.java
@@ -0,0 +1,25 @@
+import java.applet.*;
+import java.awt.*;
+/*
+<applet code=grid_demo width=400 height=400>
+</applet>
+*/
+public class grid_demo extends Applet
+{
+Label l1,l2;
+TextField t1,t2;
+BorderLayout g;
+public void init()
+{
+g=new BorderLayout();
+setLayout(g);
+l1=new Label("Enter number int first text field");
+l2=new Label("Enter number in second text field");
+//t1=new TextField(20);
+//t2=new TextField(20);
+add(l1,BorderLayout.EAST);
+//add(t1);
+add(l2,BorderLayout.EAST);
+//add(t2);
+} 
+}
