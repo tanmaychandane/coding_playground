9472efe486d45f4e62f22f41ea47d4bbd6133362
Tanmay Narendra Chandane <33379183+tanmaychandane@users.noreply.github.com>
2024-10-06 12:31:08 +0530

    Create lists.py

---
+++ python/lists.py
@@ -0,0 +1,8 @@
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Taby"]
+print(friends)
+print(friends[0])
+print(friends[-1])
+print(friends[1:])
+print(friends[1:3])
+friends[1] = "Mike"
+print(friends[1])
