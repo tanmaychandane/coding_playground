d6c14b4b3ae79981584dbcae566ecc4e027e0ce5
Tanmay Narendra Chandane <33379183+tanmaychandane@users.noreply.github.com>
2024-10-06 12:12:52 +0530

    Update building_a_basic_calculator.py

--- python/building_a_basic_calculator.py
+++ python/building_a_basic_calculator.py
@@ -9,3 +9,8 @@ num2 = input("Enter another number: ")
 result =int (num1) + int (num2)
 print(result)
 #cannot use decimals
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result =float (num1) + float (num2)
+print(result)
