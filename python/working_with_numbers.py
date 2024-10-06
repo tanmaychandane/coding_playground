140637629803b5f2b3c0c6ad38d9503f5878b6fe
Tanmay Narendra Chandane <33379183+tanmaychandane@users.noreply.github.com>
2024-10-06 11:57:24 +0530

    Create working_with_numbers.py

---
+++ python/working_with_numbers.py
@@ -0,0 +1,25 @@
+print(2)
+print(2.44)
+print(-2.32)
+print(2+4.9)
+print(2-1)
+print(200/5)
+print(17 * 2 + 5)
+print(17 * (2 + 5))
+print (10 % 3)# mod gives out reminder
+my_num = 5
+print(my_num)
+print(str(my_num))
+print(str(my_num) + " is my favorite number")
+my_num = -5
+print(abs(my_num))
+print(pow(4,6))
+print(max(3, 9))
+print(min(3,9))
+print(round(5.874))
+
+#importing math module
+from math import *
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
