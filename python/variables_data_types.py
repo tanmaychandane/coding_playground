8f5d1a076a63eb7f50b5aba55fc235ab01bbd269
Tanmay Narendra Chandane <33379183+tanmaychandane@users.noreply.github.com>
2024-10-06 11:20:50 +0530

    Update variables_data_types.py

--- python/variables_data_types.py
+++ python/variables_data_types.py
@@ -2,3 +2,12 @@ print("There once was a man named George, ")
 print("he was 70 years old. ")
 print("He really liked the name George, ")
 print("but didn't like being 90.")
+
+
+character_name = "John"
+character_age = "90"
+print("There once was a man named " +character_name +", ")
+print("he was " +character_age+ " years old. ")
+character_name = "Tom"
+print("He really liked the name "+character_name +", ")
+print("but didn't like being " +character_age+".")
