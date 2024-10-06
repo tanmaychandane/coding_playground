8bce27dc7ad3292bc0efe63aed56a5a55f39af4c
Tanmay Narendra Chandane <33379183+tanmaychandane@users.noreply.github.com>
2024-10-06 12:10:54 +0530

    Update building_a_basic_calculator.py

--- python/building_a_basic_calculator.py
+++ python/building_a_basic_calculator.py
@@ -3,3 +3,9 @@ num2 = input("Enter another number: ")
 result = num1 + num2
 print(result)
 # here it converts numbers into strings
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result =int (num1) + int (num2)
+print(result)
+#cannot use decimals
